3822eac52c39659c55ed4b82626034d3c2181ba4
ant47ii <ant47ii@googlemail.com>
2026-09-19 10:12:52 +0700

    cfg-if для определение фичи

--- joystick/src/lib.rs
+++ joystick/src/lib.rs
@@ -2,6 +2,13 @@
 mod joystick;
 pub use joystick::*;
 
-#[cfg(feature = "stm32")]
-mod joystick_stm32;
-pub use joystick_stm32::*;
+cfg_if::cfg_if! {
+	if #[cfg(feature = "stm32")] {
+		mod joystick_stm32;
+		pub use joystick_stm32::*;
+	} else if #[cfg(feature = "esp32")] {
+		todo!("esp32 не реализовано")
+	} else if #[cfg(feature = "ch32")] {
+		todo!("ch32 не реализовано")
+	}
+}
